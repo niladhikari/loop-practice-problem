8a4f33405bcf577f60b93a14b2fb75a64fd840f7
niladhikari <niladhikari445@gmail.com>
2023-07-31 11:35:44 +0600

    array loop and break add

---
+++ array-forLoop.js
@@ -0,0 +1,30 @@
+
+// numbers array
+// var numbers = [12, 23, 34, 45, 56]
+// for (let i = 0; i < numbers.length; i++) {
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+// reverse array
+// var numbers = [12, 23, 34, 45, 56]
+// for (let i =  numbers.length; i >= 0; i--) {
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+// string array
+ var items = ['laptop', 'pen', 'papers', 'mouse', 'keyborde'];
+ for (var i = 0; i < items.length; i++) {
+     var element = items[i];
+     console.log(element);
+ }
+
+// while loop array
+// var items = ['laptop', 'pen', 'papers', 'mouse', 'keyborde'];
+// var i=0
+//  while (i < items.length) {
+//     var element = items[i];
+//     console.log(element);
+//     i++;
+//  }
