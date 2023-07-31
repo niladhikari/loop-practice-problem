2cb0a1deb55c40074858700155433e707cf306f0
niladhikari <niladhikari445@gmail.com>
2023-07-31 14:47:33 +0600

    add the array some and barbie problem

---
+++ arraySum.js
@@ -0,0 +1,16 @@
+
+// for loop sum
+// var sum = 0;
+// for (let i = 1; i <= 7; i++) {
+//         sum = sum + i;
+//     }
+//     console.log(sum)
+
+//array sum
+var num = [2,3,4,5,6];
+var sum = 0;
+for (let i = 0; i < num.length; i++) {
+    var element = num[i];
+    sum = sum + element;
+}
+console.log(sum)
---
+++ barbie.js
@@ -0,0 +1,11 @@
+var friends = ['anik', 'oishik', 'rakib', 'imran', 'mahbub'];
+
+for(let i = 0; i < friends.length; i++){
+    let friend = friends[i];
+    if(friend.length % 2 == 0) {
+        console.log(friend,"Hey Barbie");
+        }
+        else{
+         continue;
+        }
+}
